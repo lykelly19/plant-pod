8fe0e89ae9dd4029b682c15034e059d1c4e124b3
Kelly <lykelly2014@icloud.com>
2020-06-28 01:15:49 -0400

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     x = "Fiona"
-    return render_template("../index.html", x=x)
+    return render_template("index.html", x=x)
 
 @app.route('/about')
 def about_page():
-    return render_template("../about.html")
+    return render_template("about.html")
 
 @app.route('/story')
 def story_page():
-    return render_template("../ourStory.html")
+    return render_template("ourStory.html")
